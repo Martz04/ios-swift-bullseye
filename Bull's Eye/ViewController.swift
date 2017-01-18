f3d201255941fe70136221c1569fb4a1268af039
Mario Alberto Gonzalez <Martz@macbook-pro-de-mario.local>
2017-01-17 18:53:36 -0600

    Initial Commit

---
+++ Bull's Eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bull's Eye
+//
+//  Created by Mario Alberto Gonzalez on 17/01/17.
+//  Copyright © 2017 Mario Alberto Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
