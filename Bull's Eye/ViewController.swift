c8491b2ef11474fc86b9f49a3d37bf00063814a4
Mario Alberto Gonzalez <Martz@MacBook-Pro-de-Mario.local>
2017-01-22 10:15:21 -0600

    Adding functionality to the game and connecting labels with variables

--- Bull's Eye/ViewController.swift
+++ Bull's Eye/ViewController.swift
@@ -10,29 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue = 50
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    @IBOutlet weak var slider:UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func startNewRound() {
+        currentValue = 50;
+        targetValue = Int(arc4random_uniform(100)) + 1
+        slider.value = Float(currentValue)
+        round += 1
+    }
+    
+    func updateLabels(){
+        targetLabel.text = "\(targetValue)"
+        scoreLabel.text = "\(score)"
+        roundLabel.text = "\(round)"
+    }
+    
+    func getTitleWithDifference(_ difference: Int) -> String {
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        return title
     }
 
     @IBAction func showMe() {
-        let alert = UIAlertController(title: "Hello world!", message: "This is my first app", preferredStyle: .alert)
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        let title = self.getTitleWithDifference(difference)
+        score += points
+        
+        let message = "You score \(points)"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        currentValue = lrounf(slider.value)
+        currentValue = lroundf(slider.value)
         print("The value of the slider is now \(slider.value)")
     }
 }
